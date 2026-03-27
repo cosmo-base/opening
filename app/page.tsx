afe2d356c6311dc56776db4f8141f8b7bb2295f5
cosmo-base <cosmobase.official@gmail.com>
2026-03-27 13:31:11 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import { HeroSection } from "@/components/hero-section"
 import { IntroductionSection } from "@/components/introduction-section"
 import { ProjectsSection } from "@/components/projects-section"
 import { FooterCtaSection } from "@/components/footer-cta-section"
+import { Countdown } from "@/components/countdown"
+
+
 
 export default function Page() {
   return (
     <main className="relative min-h-screen overflow-x-hidden">
       <HeroSection />
+      <Countdown />
       <IntroductionSection />
       <ProjectsSection />
       <FooterCtaSection />
